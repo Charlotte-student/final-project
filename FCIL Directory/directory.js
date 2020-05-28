1d2d5a8a430eec0ebe3fe3ffd01b7bb5899dbe24
Charlotte-student <62859542+Charlotte-student@users.noreply.github.com>
2020-05-27 23:40:12 -0700

    filter formatting
    added filter and card style in html.  Created beginning of array.

---
+++ FCIL Directory/directory.js
@@ -0,0 +1,79 @@
+const RESOURCES = [
+    {
+        resource: book,
+        title: International Legal Research in a Nutshell,
+        author: Marci Hoffman,
+        database: 
+        university: University of California-Berkeley,
+        ISBN:  ,
+        URL: ,
+    },
+    {
+        resource: book,
+        title: Legal Research Methods in a Modern World: a coursebook,
+        author: Paul Lomio,
+        database: none,
+        university: none,
+        ISBN: ,
+        URL: ,
+    },
+    {
+        resource: book,
+        title: International and Foreign Legal Research: a coursebook,
+        author: Marci Hoffman & Mary Rumsey,
+        database:
+        university: 
+        ISBN: ,
+        URL: , 
+    },
+    {
+        resource: book,
+        title: International Legal Research in a Global Community,
+        author: Heidi Kuehl,
+        database:
+        university:
+        ISBN: ,
+        URL: , 
+    },
+    {
+        resource: database,
+        title: GlobaLex
+        author: Lucie Olejnikova editor,
+        university:  NYU,
+        ISBN: ,
+        URL: https://nyulawglobal.org/globalex/, 
+    },
+    {
+        resource: research guide,
+        title: Foreign Comparative and International Law
+        author: various,
+        university: University of Michigan,
+        ISBN: none,
+        URL: https://libguides.law.umich.edu/fcil, 
+    },
+    {
+        resource: database,
+        title: EUR-LEX,
+        author: various,
+        university: none,
+        ISBN: none,
+        URL: https://eur-lex.europa.eu/homepage.html,
+    },
+    {
+        resource: database,
+        title: Guide to Law Online,
+        author: Library of Congress,
+        university: none,
+        ISBN: none,
+        URL: https:,
+    },
+    {
+        resource: database,
+        title: WorldLII,
+        author: various,
+        university: none,
+        ISBN: none,
+        URL: https://eur-lex.europa.eu/homepage.html,    
+    }
+
+];
